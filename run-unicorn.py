7ea6cd48edd3df0e0d2b1d480085eda323e26998
semar <semaraugusto@gmail.com>
2024-04-21 11:11:56 -0300

    can run basic mnist inference. Some cleanup in the code

--- run-unicorn.py
+++ run-unicorn.py
@@ -9,20 +9,13 @@ from unicorn.riscv_const import (
     UC_RISCV_REG_A7,
     UC_RISCV_REG_SP,
     UC_RISCV_REG_PC,
-    UC_RISCV_REG_X10,
-    UC_RISCV_REG_X11,
-    UC_RISCV_REG_X5,
-    # UC_RISCV_REG_UEPCk,
 )
 from unicorn import (
     Uc,
     UcError,
     UC_HOOK_BLOCK,
     UC_HOOK_CODE,
-    UC_HOOK_INSN,
     UC_HOOK_INTR,
-    UC_HOOK_MEM_READ,
-    UC_HOOK_MEM_WRITE,
     UC_ARCH_RISCV,
     UC_MODE_RISCV64,
     UC_MODE_RISCV32,
@@ -32,9 +25,6 @@ from unicorn import (
 from syscall_constants import ZKVMSyscalls
 from elftools.elf.elffile import ELFFile
 from pathlib import Path
-import struct
-import os
-from rangetree import RangeTree
 from capstone import (
     Cs,
     CS_ARCH_RISCV,
@@ -56,10 +46,6 @@ def hook_block(uc, address, size, user_data):
 
 # callback for tracing instructions
 def hook_code(uc, address, size, user_data):
-    # print(">>> User data: ", user_data)
-    # print(
-    #     ">>> 1 Tracing instruction at 0x%x, instruction size = 0x%x" % (address, size)
-    # )
     try:
         data = uc.mem_read(address, size)
         # print(
@@ -95,12 +81,7 @@ def hook_mem_access(uc, access, address, size, value, user_data):
         print(">>> Memory is being READ at 0x%x, data size = %u" % (address, size))
 
 
-# MAX_PROG_SIZE = 0x80000000
-# STACK_ADDR = 0x90000000
 MAX_PROG_SIZE = 0x00200000
-# STACK_ADDR = 0x00200400
-# STACK_ADDR = 0x00200400
-# static STACK_TOP: u32 = 0x0020_0400;
 
 
 def init_mem(elf_path: Path, uc: Uc):
@@ -127,37 +108,14 @@ def init_mem(elf_path: Path, uc: Uc):
         prog_dat[seg.header.p_vaddr : seg.header.p_vaddr + len(seg.data())] = seg.data()
     #
     entry = elffile.header.e_entry
-    # entry = elffile.ehdr.e_entry
     print("entrypoint: 0x%x" % entry)
 
-    # uc.mem_map(0, len(bytes(prog_dat)))
     print("max > len: ", MAX_PROG_SIZE > len(prog_dat))
     print("prog size: ", MAX_PROG_SIZE)
     print("LEN DATA: ", len(prog_dat))
     uc.mem_map(0, MAX_PROG_SIZE)
-    # STACK_ADDR = len(bytes(prog_dat)) + 0x100000
     uc.mem_write(0, bytes(prog_dat))
     print("LEN DATA: ", len(prog_dat))
-    # for seg in elffile.iter_segments():
-    #     print(f"seg: ", seg.header)
-    #
-    #     data = seg.data()
-    #     if seg.header.p_type == "PT_LOAD":
-    #         # for dd in md.disasm(data, seg.header.p_vaddr):
-    #
-    #         page_size = seg.header.p_align
-    #         mapsz = page_size * int((len(data) + page_size) / page_size)
-    #         expected_mapsz = seg.header.p_memsz
-    #         addr = seg.header.p_vaddr - (seg.header.p_vaddr % page_size)
-    #         uc.mem_map(addr, mapsz)
-    #         print(
-    #             f"[INFO] page_size {page_size} - mapsz: {mapsz} - len(data): {len(data)} - addr: {addr}"
-    #         )
-    #         print("expected_mapsz: ", expected_mapsz)
-    #
-    #         # print(f"[DEBUG] Mapping addr 0x{hex(addr)} - size: {hex(mapsz)}")
-    #         uc.mem_write(addr, data)
-    #         # print("[DEBUG] Mapping done!")
 
     print("mem has been init")
     return entry, end_addr
@@ -197,93 +155,32 @@ def hook_intr(uc, intno, user_data):
                 uc.emu_stop()
             case ZKVMSyscalls.SHA_EXTEND.value:
                 print("[SYSCALL] got syscall - `SHA_EXTEND!` IGNORING!")
-                return
+                # This seems to happen right after program exits.
+                # I think we can stop emulation here.
+                uc.emu_stop()
             case ZKVMSyscalls.SHA_COMPRESS.value:
                 print("[SYSCALL] got syscall - `SHA_COMPRESS!` IGNORING!")
-                return
             case ZKVMSyscalls.COMMIT.value:
                 print("[SYSCALL] got syscall - `COMMIT!` IGNORING!")
-                return
             case _:
                 print("[SYSCALL] got syscall 0x%x - `0x%x`???" % (intno, t0))
                 raise Exception(f"[SYSCALL] got syscall 0x{intno:x} - `0x{t0:x}`???")
-        # return
-        # x11 = uc.reg_read(UC_RISCV_REG_X11)
-        # name = md.insn_name(intno)
-        # print("[SYSCALL] t0: 0x%x" % t0)
-        # print("[SYSCALL] a0: 0x%x" % a0)
-        # print("[SYSCALL] a1: 0x%x" % a1)
-        # print("[SYSCALL] a2: 0x%x" % a2)
-        # print("[SYSCALL] a0: 0x%x" % a0)
-        # print("[SYSCALL] a2: 0x%x" % a2)
-        # print("[SYSCALL] a7: 0x%x" % a7)
     except Exception as e:
         print("[instr] failed to get instr name for got syscall 0x%x ???" % intno)
         print("[instr] Error: ", e)
 
-    # if intno != 0x80:
-    # print("[instr] got interrupt 0x%x ???" % intno)
-    # uc.emu_stop()
-    # return
-
-
-def hook_syscall(uc, user_data):
-    # only handle Linux syscall
-    try:
-        x5 = uc.reg_read(UC_RISCV_REG_X5)
-        t0 = uc.reg_read(UC_RISCV_REG_T0)
-        x10 = uc.reg_read(UC_RISCV_REG_X10)
-        x11 = uc.reg_read(UC_RISCV_REG_X11)
-        # name = md.insn_name(intno)
-        # print("[SYSCALL] got syscall 0x%x - `%s`???" % (intno, name))
-        print("[SYSCALL] x5: 0x%x" % x5)
-        print("[SYSCALL] t0: 0x%x" % t0)
-        print("[SYSCALL] x10: 0x%x" % x10)
-        print("[SYSCALL] x11: 0x%x" % x11)
-    except Exception as e:
-        print("[SYSCALL] failed to get instr name for got syscall 0x%x ???" % intno)
-        print("[SYSCALL] Error: ", e)
-
-    # if intno != 0x80:
-    #     print("[SYSCALL] got interrupt 0x%x ???" % intno)
-    #     uc.emu_stop()
-    #     return
-
 
 def run(uc: Uc, entry: int = 0x0, end_addr: int = 0x0):
-    start_addr = 0
-    # start_addr = 0x10278
-    # end_addr = 0x101F0
-    # end_addr = 0x102CC
-    # tracing all basic blocks with customized callback
-    # elf_path = Path("riscv")
-    # elf_path = Path(
-    #     "../rust-cross/target/riscv32im-succinct-zkvm-elf/release/rust-cross"
-    # )
-    # elffile = ELFFile(elf_path.open("rb"))
-    # entry = elffile.header.e_entry
-    # print("entrypoint: 0x%x" % entry)
-    # end_addr = 0
-    # for seg in elffile.iter_segments():
-    #     end_addr = max(end_addr, seg.header.p_vaddr + seg.header.p_memsz)
     uc.hook_add(UC_HOOK_BLOCK, hook_block)
 
     # tracing all instructions with customized callback
     uc.hook_add(UC_HOOK_CODE, hook_code)
     uc.hook_add(UC_HOOK_INTR, hook_intr)
-    # uc.hook_add(UC_HOOK_INSN, hook_syscall, None, 1, 0, UC_RISCV_REG_A7)
-    # uc.hook_add(UC_HOOK_INSN, hook_syscall)
-    # uc.hook_add(UC_HOOK_CODE, hook_code)
-    # uc.hook_add(UC_HOOK_INSN, hook_in)
-    # uc.hook_add(UC_HOOK_INSN, hook_out, None, 1, 0, UC_X86_INS_OUT)
 
-    # emulate machine code in infinite time
-    # uc.emu_start(start_addr, end_addr)
     print("entry: 0x%x" % entry)
     print("end_addr: ", end_addr)
     # uc.emu_start(entry, end_addr)
-    uc.emu_start(entry, end_addr, count=0x100000)
-    # uc.emu_start(entry, 0x13A6C, count=0x1000)
+    uc.emu_start(entry, end_addr, count=0x100000000)
 
 
 # Test RISCV
