09705c4824730dc58c81ff9c00544ac8cd6d9156
semar <semaraugusto@gmail.com>
2024-04-30 18:43:47 -0300

    mnist model loading

--- run-unicorn.py
+++ run-unicorn.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import struct
+import numpy as np
 from unicorn.riscv_const import (
     UC_RISCV_REG_A0,
     UC_RISCV_REG_T0,
+    UC_RISCV_REG_T3,
     UC_RISCV_REG_A1,
     UC_RISCV_REG_A2,
     UC_RISCV_REG_A5,
@@ -21,9 +23,20 @@ from unicorn import (
     UC_MODE_RISCV64,
     UC_MODE_RISCV32,
     UC_MEM_WRITE,
+    UC_MEM_WRITE,
+    UC_MEM_READ,
+    UC_MEM_FETCH,
+    UC_MEM_READ_UNMAPPED,
+    UC_MEM_WRITE_UNMAPPED,
+    UC_MEM_FETCH_UNMAPPED,
+    UC_MEM_WRITE_PROT,
+    UC_MEM_FETCH_PROT,
+    UC_MEM_READ_AFTER,
+    UC_HOOK_MEM_INVALID,
 )
 
-from syscall_constants import ZKVMSyscalls
+from safetensors import safe_open
+from syscall_constants import RiscvSyscalls, ZKVMSyscalls
 from elftools.elf.elffile import ELFFile
 from pathlib import Path
 from capstone import (
@@ -57,15 +70,15 @@ def hook_code(uc, address, size, user_data):
             return
         # for dd in md.disasm(data, address):
         #     sp = uc.reg_read(UC_RISCV_REG_SP)
-        # if dd.mnemonic == "ecall":
-        #     print(
-        #         f"[TRACING] 0x{dd.address:x}:\t{dd.mnemonic}\t{dd.op_str}: sp_val: 0x{sp:x}"
-        #         # f"[TRACING] 0x{dd.address:x}:\t{dd.mnemonic}\t{dd.op_str}"
-        #     )
-        # print(
-        #     f"[TRACING] 0x{dd.address:x}:\t{dd.mnemonic}\t{dd.op_str}: sp_val: 0x{sp:x}"
-        #     # f"[TRACING] 0x{dd.address:x}:\t{dd.mnemonic}\t{dd.op_str}"
-        # )
+        #     if dd.mnemonic == "ecall":
+        #         print(
+        #             f"[TRACING] 0x{dd.address:x}:\t{dd.mnemonic}\t{dd.op_str}: sp_val: 0x{sp:x}"
+        #             # f"[TRACING] 0x{dd.address:x}:\t{dd.mnemonic}\t{dd.op_str}"
+        #         )
+    #         print(
+    #             f"[TRACING] 0x{dd.address:x}:\t{dd.mnemonic}\t{dd.op_str}: sp_val: 0x{sp:x}"
+    #             # f"[TRACING] 0x{dd.address:x}:\t{dd.mnemonic}\t{dd.op_str}"
+    #         )
     except Exception as e:
         print(f"[TRACING] Error: {e}")
         pass
@@ -86,7 +99,10 @@ MAX_PROG_SIZE = 0x10000000
 TOKENIZER_START = MAX_PROG_SIZE
 TOKENIZER_MAX_SIZE = 0x206000
 TOKENIZER_END = TOKENIZER_START + TOKENIZER_MAX_SIZE  # 0x20206000
-STACK_START = TOKENIZER_END
+MODEL_START = TOKENIZER_END
+MODEL_MAX_SIZE = 0x2000_0000
+MODEL_END = MODEL_START + MODEL_MAX_SIZE
+STACK_START = MODEL_END
 
 
 def load_tokenizer(tokenizer_path: Path, uc: Uc):
@@ -100,35 +116,64 @@ def load_tokenizer(tokenizer_path: Path, uc: Uc):
     uc.mem_map(TOKENIZER_START, TOKENIZER_MAX_SIZE)
     print("done mapping tokenizer")
     print(f"writing len {len(tokenizer_bytes)=} of tokenizer to 0x%x" % TOKENIZER_START)
-    # print(f"writing len {hex(tokenizer_bytes)=} of tokenizer to 0x%x" % TOKENIZER_START)
-    # print(
-    #     f"writing len {bytes(len(tokenizer_bytes))} of tokenizer to 0x%x"
-    #     % TOKENIZER_START
-    # )
     print(f"writing len {len(tokenizer_bytes)=} of tokenizer to 0x%x" % TOKENIZER_START)
     packed_bytes = struct.pack(f"<{len(tokenizer_bytes)}s", bytearray(tokenizer_bytes))
-    # packed_bytes_be = struct.pack(">s", tokenizer_bytes)
-    # print("Tokenizer starting bytes[:4]: ", int.from_bytes(packed_bytes_be[:4]))
-    # print("Tokenizer starting bytes[4:8]: ", int.from_bytes(packed_bytes_be[4:8]))
-    # print("Tokenizer starting bytes[8:12]: ", int.from_bytes(packed_bytes_be[8:12]))
-    print("[HERE] bytes[0:12]: ", tokenizer_bytes[:50])
-    # print("[HERE] bytes_be[0:12]: ", packed_bytes_be[:12])
-    # print("[HERE] bytes_le[0:12]: ", packed_bytes[:12])
-    # print("[HERE] bytes_le[0:12]: ", packed_bytes)
-    # print("[HERE] bytes[0]: ", int(tokenizer_bytes[0]))
-    # print("[HERE] bytes[1]: ", int(tokenizer_bytes[1]))
-    # print("[HERE] bytes[2]: ", int(tokenizer_bytes[2]))
-    # print("[HERE] bytes[3]: ", int(tokenizer_bytes[3]))
-    # print("[HERE] bytes[4]: ", int(tokenizer_bytes[4]))
-    # print("[HERE] bytes[5]: ", int(tokenizer_bytes[5]))
-    # print("[HERE] bytes[6]: ", int(tokenizer_bytes[6]))
-    # print("[HERE] bytes[7]: ", int(tokenizer_bytes[7]))
-    # print("[HERE] bytes[8]: ", int(tokenizer_bytes[7]))
-    # print("[HERE] bytes[8]: ", int(tokenizer_bytes[7]))
     print("[HERE] bytes[0:12]: ", list(map(int, packed_bytes[:12])))
     uc.mem_write(TOKENIZER_START, struct.pack("<I", 0x67676D6C))
     uc.mem_write(TOKENIZER_START + 4, struct.pack("<I", len(tokenizer_bytes)))
     uc.mem_write(TOKENIZER_START + 8, packed_bytes)
+    print("done loading tokenizer")
+
+def load_model(model_path: Path, uc: Uc):
+    global MODEL_START, MODEL_MAX_SIZE
+    print("loading tokenizer")
+    # tokenizer_bytes = tokenizer_path.open("rb").read()
+    addr = MODEL_START
+    uc.mem_map(MODEL_START, MODEL_MAX_SIZE)
+    with safe_open(model_path, framework="pt", device="cpu") as f:
+        num_tensors = len(f.keys())
+        print(f"num_tensors: {num_tensors}")
+        uc.mem_write(addr, struct.pack("<I", 0x67676D6C))
+        addr += 4
+        uc.mem_write(addr, struct.pack("<I", num_tensors))
+        addr += 4
+        for key in f.keys():
+            tensor = f.get_tensor(key)
+            print(f"key: {key}, tensor: {tensor}")
+            name_bytes = struct.pack(f"<{len(key)}s", key.encode("utf-8"))
+            name_len = len(name_bytes)
+            uc.mem_write(addr, struct.pack("<I", name_len))
+            addr += 4
+            uc.mem_write(addr, name_bytes)
+            addr += name_len
+            print(f"name_len: {name_len}, name_bytes: {name_bytes}")
+            uc.mem_write(addr, struct.pack("<I", tensor.dim()))
+            addr += 4
+            print(f"tensor_dim: {tensor.dim()}")
+            for dim in range(tensor.dim()):
+                uc.mem_write(addr, struct.pack("<I", tensor.shape[dim]))
+                addr += 4
+                print(f"tensor.shape[dim]: {tensor.shape[dim]}")
+
+            tensor_bytes = tensor.numpy().astype(np.float32).tobytes()
+            tensor_packed = struct.pack(f"<{len(tensor_bytes)}s", tensor_bytes)
+            uc.mem_write(addr, tensor_packed)
+            addr += len(tensor_packed)
+
+
+    # assert len(tokenizer_bytes) + 4 <= TOKENIZER_MAX_SIZE, "INCONSISTENT TOKENIZER SIZE"
+    # print(
+    #     f"Mapping tokenizer addr 0x{hex(TOKENIZER_START)} - size: {hex(len(tokenizer_bytes))}"
+    # )
+    # uc.mem_map(TOKENIZER_START, TOKENIZER_MAX_SIZE)
+    # print("done mapping tokenizer")
+    # print(f"writing len {len(tokenizer_bytes)=} of tokenizer to 0x%x" % TOKENIZER_START)
+    # print(f"writing len {len(tokenizer_bytes)=} of tokenizer to 0x%x" % TOKENIZER_START)
+    # packed_bytes = struct.pack(f"<{len(tokenizer_bytes)}s", bytearray(tokenizer_bytes))
+    # print("[HERE] bytes[0:12]: ", list(map(int, packed_bytes[:12])))
+    uc.mem_write(MODEL_START, struct.pack("<I", 0x67676D6C))
+    # uc.mem_write(MODEL_START + 4, struct.pack("<I", len(tokenizer_bytes)))
+    # uc.mem_write(MODEL_START + 8, packed_bytes)
     print("done loading tokenizer")
 
 
@@ -172,10 +217,11 @@ def init_mem(elf_path: Path, uc: Uc):
 def init_stack(uc: Uc):
     global TOKENIZER_END
     # stack_addr = STACK_ADDR
-    stack_addr = TOKENIZER_END
-    stack_pointer = stack_addr + 0x400
-    stack_size = 0x2000000
+    stack_addr = MODEL_END + 0x1000
+    stack_size = 0x20000000
+    stack_pointer = stack_addr + (stack_size//2)
     print(f"Mapping stack addr 0x{hex(stack_addr)} - size: {hex(stack_size)}")
+    uc.reg_write(UC_RISCV_REG_SP, stack_pointer)
     uc.mem_map(stack_addr, stack_size)
     print("Stack has been init")
 
@@ -187,7 +233,19 @@ def hook_intr(uc, intno, user_data):
         a0 = uc.reg_read(UC_RISCV_REG_A0)  # fd
         a1 = uc.reg_read(UC_RISCV_REG_A1)  # addr
         a2 = uc.reg_read(UC_RISCV_REG_A2)  # size
+        # a7 = uc.reg_read(UC_RISCV_REG_A7)  # fd
+        # print("[SYSCALL] t0: 0x%x a7: 0x%x a7: %d:" % (t0, a7, a7))
+        # print("[SYSCALL] t0: 0x%x a7: 0x%x a7: %d: args(%d %d %d)" % (t0, a7, a7, a0, a1, a2))
         match t0:
+        # match a7:
+            # case RiscvSyscalls.SYS_getuid.value:
+            #     print("[SYSCALL] `SYS_getuid!` IGNORING!")
+            # case RiscvSyscalls.SYS_geteuid.value:
+            #     print("[SYSCALL] `SYS_geteuid!` IGNORING!")
+            # case RiscvSyscalls.SYS_geteid.value:
+            #     print("[SYSCALL] `SYS_geteid!` IGNORING!")
+            # case RiscvSyscalls.SYS_getegid.value:
+            #     print("[SYSCALL] `SYS_getegid!` IGNORING!")
             # case 0x2:
             case ZKVMSyscalls.WRITE.value:
                 # if t0 == 0x2:
@@ -205,15 +263,62 @@ def hook_intr(uc, intno, user_data):
                 uc.emu_stop()
             case ZKVMSyscalls.SHA_COMPRESS.value:
                 print("[SYSCALL] [SHA_COMPRESS!] - `SHA_COMPRESS!` IGNORING!")
-            case ZKVMSyscalls.COMMIT.value:
-                print("[SYSCALL] [COMMIT!] - `COMMIT!` IGNORING!")
             case _:
                 print("[SYSCALL] [UNKNOWN] 0x%x - `0x%x`???" % (intno, t0))
-                raise Exception(f"[SYSCALL] [UNKNOWN] 0x{intno:x} - `0x{t0:x}`???")
+                # raise Exception(f"[SYSCALL] [UNKNOWN] 0x{intno:x} - `0x{t0:x}`???")
+        # match t0:
+        #     # case 0x2:
+        #     case ZKVMSyscalls.WRITE.value:
+        #         # if t0 == 0x2:
+        #         data = uc.mem_read(a1, a2)
+        #         print("[SYSCALL] [WRITE] `%s`" % data.decode("utf-8").strip())
+        #     # case 0x0:
+        #     case ZKVMSyscalls.HALT.value:
+        #         # data = uc.mem_read(a1, a2)
+        #         print("[SYSCALL] [HALT!]")
+        #         uc.emu_stop()
+        #     case ZKVMSyscalls.SHA_EXTEND.value:
+        #         # This seems to happen right after program exits.
+        #         # I think we can stop emulation here.
+        #         print("[SYSCALL] [SHA_EXTEND!]. EXECUTION IS DONE. HALTING!")
+        #         uc.emu_stop()
+        #     case ZKVMSyscalls.SHA_COMPRESS.value:
+        #         print("[SYSCALL] [SHA_COMPRESS!] - `SHA_COMPRESS!` IGNORING!")
+        #     case ZKVMSyscalls.COMMIT.value:
+        #         print("[SYSCALL] [COMMIT!] - `COMMIT!` IGNORING!")
+        #     case _:
+        #         print("[SYSCALL] [UNKNOWN] 0x%x - `0x%x`???" % (intno, t0))
+        #         # raise Exception(f"[SYSCALL] [UNKNOWN] 0x{intno:x} - `0x{t0:x}`???")
     except Exception as e:
-        print("[instr] failed to get instr name for got syscall 0x%x ???" % intno)
+        print("[instr] failed to get instr name for got intno 0x%x ???" % intno)
+        print("[instr] failed to get instr name for got syscall 0x%x ???" % t0)
         print("[instr] Error: ", e)
 
+
+def hook_mem_invalid(uc, access, address, size, value, user_data):
+    """For Debugging Use Only"""
+    eip = uc.reg_read(UC_RISCV_REG_PC)
+    t3 = uc.reg_read(UC_RISCV_REG_T3)
+    print("T3: 0x%x" % t3)
+    if access == UC_MEM_WRITE:
+        print("invalid WRITE of 0x%x at 0x%X, data size = %u, data value = 0x%x" % (address, eip, size, value))
+    if access == UC_MEM_READ:
+        print("invalid READ of 0x%x at 0x%X, data size = %u" % (address, eip, size))
+    if access == UC_MEM_FETCH:
+        print("UC_MEM_FETCH of 0x%x at 0x%X, data size = %u" % (address, eip, size))
+    if access == UC_MEM_READ_UNMAPPED:
+        print("UC_MEM_READ_UNMAPPED of 0x%x at 0x%X, data size = %u" % (address, eip, size))
+    if access == UC_MEM_WRITE_UNMAPPED:
+        print("UC_MEM_WRITE_UNMAPPED of 0x%x at 0x%X, data size = %u" % (address, eip, size))
+    if access == UC_MEM_FETCH_UNMAPPED:
+        print("UC_MEM_FETCH_UNMAPPED of 0x%x at 0x%X, data size = %u" % (address, eip, size))
+    if access == UC_MEM_WRITE_PROT:
+        print("UC_MEM_WRITE_PROT of 0x%x at 0x%X, data size = %u" % (address, eip, size))
+    if access == UC_MEM_FETCH_PROT:
+        print("UC_MEM_FETCH_PROT of 0x%x at 0x%X, data size = %u" % (address, eip, size))
+    if access == UC_MEM_READ_AFTER:
+        print("UC_MEM_READ_AFTER of 0x%x at 0x%X, data size = %u" % (address, eip, size))
+    return False
 
 def run(uc: Uc, entry: int = 0x0, end_addr: int = 0x0):
     uc.hook_add(UC_HOOK_BLOCK, hook_block)
@@ -221,13 +326,20 @@ def run(uc: Uc, entry: int = 0x0, end_addr: int = 0x0):
     # tracing all instructions with customized callback
     uc.hook_add(UC_HOOK_CODE, hook_code)
     uc.hook_add(UC_HOOK_INTR, hook_intr)
+    uc.hook_add(UC_HOOK_MEM_INVALID, hook_mem_invalid)
 
     print("entry: 0x%x" % entry)
     print("end_addr: ", end_addr)
     # uc.emu_start(entry, end_addr)
     SECOND = 1_000_000  # (unicorn uses microseconds apparently)
+    MINUTE = 60 * SECOND
     try:
-        uc.emu_start(entry, end_addr, timeout=5 * SECOND)
+        # uc.reg_write(UC_RISCV_REG_PC, entry)
+        # entry = 0x000ce002a
+        # uc.reg_write(UC_RISCV_REG_PC, entry)
+        # uc.emu_start(entry, end_addr, timeout=1 * MINUTE, count=0x200)
+        uc.emu_start(entry, end_addr, timeout=30 * SECOND)
+        # uc.emu_start(entry, end_addr, timeout=1 * MINUTE)
     except KeyboardInterrupt as e:
         print("KeyboardInterrupt: %s" % e)
         uc.emu_stop()
@@ -237,19 +349,25 @@ def run(uc: Uc, entry: int = 0x0, end_addr: int = 0x0):
 def test_riscv():
     print("Emulate RISCV code")
     uc = Uc(UC_ARCH_RISCV, UC_MODE_RISCV32 + CS_MODE_LITTLE_ENDIAN)
+    # uc = Uc(UC_ARCH_RISCV, UC_MODE_RISCV64 + CS_MODE_LITTLE_ENDIAN)
     try:
         # Initialize emulator in RISCV64 mode
         # mu = Uc(UC_ARCH_RISCV, UC_MODE_RISCV64 + CS_MODE_LITTLE_ENDIAN)
+        model_path = Path(
+            "./linear.safetensors"
+        )
 
         elf_path = Path(
             "../rust-cross/target/riscv32im-succinct-zkvm-elf/release/rust-cross"
             # "../rust-cross/target/riscv64gc-unknown-none-elf/release/rust-cross"
+            # "../rust-riscv/target/riscv64gc-unknown-linux-gnu/release/rust-riscv"
+            # "../rust-riscv/target/riscv64gc-unknown-linux-musl/release/rust-riscv"
         )
         tokenizer_path = Path(
             "/home/semar/.cache/huggingface/hub/models--stabilityai--stablelm-3b-4e1t/snapshots/fa4a6a92fca83c3b4223a3c9bf792887090ebfba/tokenizer.json"
         )
         entry, end_addr = init_mem(elf_path, uc)
-        load_tokenizer(tokenizer_path, uc)
+        load_model(model_path, uc)
         init_stack(uc)
         # now print out some registers
         run(uc, entry, end_addr)
--- syscall_constants.py
+++ syscall_constants.py
@@ -7,3 +7,13 @@ class ZKVMSyscalls(Enum):
     SHA_EXTEND = 0x300105
     SHA_COMPRESS = 0x10106
     COMMIT = 0x10
+
+class RiscvSyscalls(Enum):
+    # SYS_geteid = 0xae
+    # SYS_geteuid = 0xaf
+    SYS_getuid = 174
+    SYS_geteid = 175
+    SYS_geteuid = 176
+    SYS_getegid = 177
+    SYS_gettid = 178
+    SYS_sysinfo = 179
